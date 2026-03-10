82bd864dbc55bba98c302b604f8151077bee870a
RobertoPSF <r.filho1@hotmail.com>
2026-03-10 15:12:43 -0300

    chore: creating envvars

--- app/database.py
+++ app/database.py
@@ -1,8 +1,13 @@
 import time
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/events_db"
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
